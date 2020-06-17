cfaba8a067d612b71793c2769b97e8177ce80e82
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2020-06-17 14:13:27 +0900

    管理者コントローラークラスにメソッドを追加

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,13 +1,20 @@
 package jp.co.sample.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
 import jp.co.sample.service.AdministratorService;
 
+/**
+ * 管理者情報登録画面を表示する処理を記述する
+ * @author konomitaimu
+ *
+ */
 @Controller
 @RequestMapping("/")
 public class AdministratorController {
@@ -33,5 +40,22 @@ public class AdministratorController {
 	@RequestMapping("/toInsert")
 	public String toInsert() {
 		return "administrator/insert";
+	}
+	
+	/**
+	 * 管理者情報を登録する
+	 * @param form
+	 * @return 「/」にリダイレクト
+	 */
+	@RequestMapping("/insert")
+	public String insert(InsertAdministratorForm form) {
+		Administrator adm = new Administrator();
+		adm.setName(form.getName());
+		adm.setMailAddress(form.getMailAddress());
+		adm.setPassword(form.getPassword());
+		
+		administratorService.insert(adm);
+		
+		return "redirect:/";
 	}
 }
