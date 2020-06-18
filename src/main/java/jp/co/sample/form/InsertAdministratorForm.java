2ef6f611a289478b7cae633c10819e0e832a72cd
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2020-06-18 16:24:06 +0900

    管理者フォームクラスに入力値チェック用アノテーションを追加

--- src/main/java/jp/co/sample/form/InsertAdministratorForm.java
+++ src/main/java/jp/co/sample/form/InsertAdministratorForm.java
@@ -1,12 +1,20 @@
 package jp.co.sample.form;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class InsertAdministratorForm {
 	
 	/** 名前 */
+	@NotBlank(message="名前は必須です")
 	private String name;
 	/** メールアドレス */
+	@Size(min=1, max=100, message="Eメールは1文字以上100文字以内で記載してください")
+	@Email(message="Eメールの形式が不正です")
 	private String mailAddress;
 	/** パスワード */
+	@Size(min=1, max=10, message="パスワードは1文字以上10文字以内で記載してください")
 	private String password;
 	
 	@Override
