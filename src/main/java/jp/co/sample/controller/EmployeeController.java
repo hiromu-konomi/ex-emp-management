fe098caeeb6d88e11a1d662687ff970a8848654e
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2020-06-18 10:54:15 +0900

    従業員コントローラーの作成

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package jp.co.sample.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Employee;
+import jp.co.sample.form.UpdateEmployeeForm;
 import jp.co.sample.service.EmployeeService;
 
 import java.util.ArrayList;
@@ -17,9 +20,15 @@ public class EmployeeController {
 
 	@Autowired
 	private EmployeeService employeeService;
-	
+
+	@ModelAttribute
+	public UpdateEmployeeForm setUpUpdateEmployeeForm() {
+		return new UpdateEmployeeForm();
+	}
+
 	/**
 	 * 従業員一覧を出力する
+	 * 
 	 * @param model
 	 * @return 従業員情報一覧
 	 */
@@ -29,5 +38,12 @@ public class EmployeeController {
 		employeeList = employeeService.showList();
 		model.addAttribute("employeeList", employeeList);
 		return "employee/list";
+	}
+
+	@RequestMapping("/showDetail")
+	public String showDetail(String id, Model model) {
+		Employee emp = employeeService.showDetail(Integer.parseInt(id));
+		model.addAttribute("employee", emp);
+		return "employee/detail";
 	}
 }
